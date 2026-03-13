2b94b6818b9c8baf8d34c0bc91f091adb12f9d58
axtk <axtkx@inbox.ru>
2026-03-13 13:30:23 +0300

    replace appstage's dir with express static

--- src/entries/files-demo/server.ts
+++ src/entries/files-demo/server.ts
@@ -1,14 +1,12 @@
-import { dir } from "appstage";
 import { static as expressStatic, Router } from "express";
 
 export let server = Router();
 
 server.use("/files-demo/-", expressStatic("src/entries/files-demo/public"));
 
-server.get(
-  "/files-demo{/:name}",
-  dir({
-    path: "src/entries/files-demo/content",
-    name: (req) => String(req.params.name ?? "") || "intro",
+server.use(
+  "/files-demo",
+  expressStatic("src/entries/files-demo/content", {
+    extensions: ["html"],
   }),
 );
--- src/entries/main/server.ts
+++ src/entries/main/server.ts
@@ -1,14 +1,6 @@
-import { dir } from "appstage";
 import { static as expressStatic, Router } from "express";
 
 export let server = Router();
 
 server.use("/main/-", expressStatic("src/entries/main/public"));
-
-server.get(
-  "/",
-  dir({
-    path: "src/entries/main",
-    name: "index",
-  }),
-);
+server.use("/", expressStatic("src/entries/main/content"));
